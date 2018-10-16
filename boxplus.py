6aaea83a927ab68a13bdcdad58fd9b5231408355
YSH4807 <yuan.yuan1@baozun.com>
2018-10-16 11:14:48 +0800

    警告移除

--- boxplus.py
+++ boxplus.py
@@ -17,47 +17,47 @@ class SysTrayIcon(object):
     SPECIAL_ACTIONS = [QUIT]
     FIRST_ID = 1314
 
-    def __init__(s,
+    def __init__(self,
                  icon,
                  hover_text,
                  menu_options,
                  on_quit=None,
                  default_menu_index=None,
                  window_class_name=None,):
-        s.icon = icon
-        s.hover_text = hover_text
-        s.on_quit = on_quit
+        self.icon = icon
+        self.hover_text = hover_text
+        self.on_quit = on_quit
 
-        menu_options = menu_options + (('退出', None, s.QUIT),)
-        s._next_action_id = s.FIRST_ID
-        s.menu_actions_by_id = set()
-        s.menu_options = s._add_ids_to_menu_options(list(menu_options))
-        s.menu_actions_by_id = dict(s.menu_actions_by_id)
-        del s._next_action_id
+        menu_options = menu_options + (('退出', None, self.QUIT),)
+        self._next_action_id = self.FIRST_ID
+        self.menu_actions_by_id = set()
+        self.menu_options = self._add_ids_to_menu_options(list(menu_options))
+        self.menu_actions_by_id = dict(self.menu_actions_by_id)
+        del self._next_action_id
 
-        s.default_menu_index = (default_menu_index or 0)
-        s.window_class_name = window_class_name or "SysTrayIconPy"
+        self.default_menu_index = (default_menu_index or 0)
+        self.window_class_name = window_class_name or "SysTrayIconPy"
 
-        message_map = {win32gui.RegisterWindowMessage("TaskbarCreated"): s.refresh_icon,
-                       win32con.WM_DESTROY: s.destroy,
-                       win32con.WM_COMMAND: s.command,
-                       win32con.WM_USER+20 : s.notify,}
+        message_map = {win32gui.RegisterWindowMessage("TaskbarCreated"): self.refresh_icon,
+                       win32con.WM_DESTROY: self.destroy,
+                       win32con.WM_COMMAND: self.command,
+                       win32con.WM_USER+20 : self.notify,}
         # 注册窗口类。
         window_class = win32gui.WNDCLASS()
         window_class.hInstance = win32gui.GetModuleHandle(None)
-        window_class.lpszClassName = s.window_class_name
+        window_class.lpszClassName = self.window_class_name
         window_class.style = win32con.CS_VREDRAW | win32con.CS_HREDRAW
         window_class.hCursor = win32gui.LoadCursor(0, win32con.IDC_ARROW)
         window_class.hbrBackground = win32con.COLOR_WINDOW
         window_class.lpfnWndProc = message_map  # 也可以指定wndproc.
-        s.classAtom = win32gui.RegisterClass(window_class)
+        self.classAtom = win32gui.RegisterClass(window_class)
 
-    def show_icon(s):
+    def show_icon(self):
         # 创建窗口.
         hinst = win32gui.GetModuleHandle(None)
         style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        s.hwnd = win32gui.CreateWindow(s.classAtom,
-                                          s.window_class_name,
+        self.hwnd = win32gui.CreateWindow(self.classAtom,
+                                          self.window_class_name,
                                           style,
                                           0,
                                           0,
@@ -67,45 +67,46 @@ class SysTrayIcon(object):
                                           0,
                                           hinst,
                                           None)
-        win32gui.UpdateWindow(s.hwnd)
-        s.notify_id = None
-        s.refresh_icon()
+        win32gui.UpdateWindow(self.hwnd)
+        self.notify_id = None
+        self.refresh_icon()
         win32gui.PumpMessages()
 
-    def show_menu(s):
+    def show_menu(self):
         menu = win32gui.CreatePopupMenu()
-        s.create_menu(menu, s.menu_options)
+        self.create_menu(menu, self.menu_options)
         # win32gui.SetMenuDefaultItem(menu, 1000, 0)
 
         pos = win32gui.GetCursorPos()
         # See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/menus_0hdi.asp
-        win32gui.SetForegroundWindow(s.hwnd)
+        win32gui.SetForegroundWindow(self.hwnd)
         win32gui.TrackPopupMenu(menu,
                                 win32con.TPM_LEFTALIGN,
                                 pos[0],
                                 pos[1],
                                 0,
-                                s.hwnd,
+                                self.hwnd,
                                 None)
-        win32gui.PostMessage(s.hwnd, win32con.WM_NULL, 0, 0)
+        win32gui.PostMessage(self.hwnd, win32con.WM_NULL, 0, 0)
 
-    def destroy(s, hwnd, msg, wparam, lparam):
-        if s.on_quit: s.on_quit(s) #运行传递的on_quit
-        nid = (s.hwnd, 0)
+    def destroy(self, hwnd, msg, wparam, lparam):
+        if self.on_quit: self.on_quit(self)  # 运行传递的on_quit
+        nid = (self.hwnd, 0)
         win32gui.Shell_NotifyIcon(win32gui.NIM_DELETE, nid)
-        win32gui.PostQuitMessage(0) # 退出托盘图标
+        win32gui.PostQuitMessage(0)  # 退出托盘图标
 
-    def notify(s, hwnd, msg, wparam, lparam):
-        if lparam == win32con.WM_LBUTTONDBLCLK: # 双击左键
+    def notify(self, hwnd, msg, wparam, lparam):
+        if lparam == win32con.WM_LBUTTONDBLCLK:  # 双击左键
             # s.execute_menu_option(s.default_menu_index + s.FIRST_ID)
             pass
-        elif lparam == win32con.WM_RBUTTONUP: # 单击右键
-            s.show_menu()
-        elif lparam == win32con.WM_LBUTTONUP: # 单击左键
-            nid = (s.hwnd, 0)
+        elif lparam == win32con.WM_RBUTTONUP:  # 单击右键
+            self.show_menu()
+        elif lparam == win32con.WM_LBUTTONUP:  # 单击左键
+            nid = (self.hwnd, 0)
             win32gui.Shell_NotifyIcon(win32gui.NIM_DELETE, nid)
-            win32gui.PostQuitMessage(0) # 退出托盘图标
-            if Main: Main.root.deiconify()
+            win32gui.PostQuitMessage(0)  # 退出托盘图标
+            if Main:
+                Main.root.deiconify()
         return True
         """ 可能的鼠标事件：
         WM_MOUSEMOVE
@@ -119,27 +120,27 @@ class SysTrayIcon(object):
         WM_MBUTTONUP
         WM_MBUTTONDBLCLK"""
 
-    def _add_ids_to_menu_options(s, menu_options):
+    def _add_ids_to_menu_options(self, menu_options):
         result = []
         for menu_option in menu_options:
             option_text, option_icon, option_action = menu_option
-            if callable(option_action) or option_action in s.SPECIAL_ACTIONS:
-                s.menu_actions_by_id.add((s._next_action_id, option_action))
-                result.append(menu_option + (s._next_action_id,))
+            if callable(option_action) or option_action in self.SPECIAL_ACTIONS:
+                self.menu_actions_by_id.add((self._next_action_id, option_action))
+                result.append(menu_option + (self._next_action_id,))
             else:
                 result.append((option_text,
                                option_icon,
-                               s._add_ids_to_menu_options(option_action),
-                               s._next_action_id))
-            s._next_action_id += 1
+                               self._add_ids_to_menu_options(option_action),
+                               self._next_action_id))
+            self._next_action_id += 1
         return result
 
-    def refresh_icon(s, **data):
+    def refresh_icon(self, **data):
         hinst = win32gui.GetModuleHandle(None)
-        if os.path.isfile(s.icon):  # 尝试找到自定义图标
+        if os.path.isfile(self.icon):  # 尝试找到自定义图标
             icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
             hicon = win32gui.LoadImage(hinst,
-                                       s.icon,
+                                       self.icon,
                                        win32con.IMAGE_ICON,
                                        0,
                                        0,
@@ -147,22 +148,22 @@ class SysTrayIcon(object):
         else:  # 找不到图标文件 - 使用默认值
             hicon = win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
 
-        if s.notify_id: message = win32gui.NIM_MODIFY
+        if self.notify_id: message = win32gui.NIM_MODIFY
         else: message = win32gui.NIM_ADD
-        s.notify_id = (s.hwnd,
+        self.notify_id = (self.hwnd,
                           0,
                           win32gui.NIF_ICON | win32gui.NIF_MESSAGE | win32gui.NIF_TIP,
                           win32con.WM_USER+20,
                           hicon,
-                          s.hover_text)
-        win32gui.Shell_NotifyIcon(message, s.notify_id)
+                          self.hover_text)
+        win32gui.Shell_NotifyIcon(message, self.notify_id)
 
-    def create_menu(s, menu, menu_options):
+    def create_menu(self, menu, menu_options):
         for option_text, option_icon, option_action, option_id in menu_options[::-1]:
             if option_icon:
-                option_icon = s.prep_menu_icon(option_icon)
+                option_icon = self.prep_menu_icon(option_icon)
 
-            if option_id in s.menu_actions_by_id:
+            if option_id in self.menu_actions_by_id:
                 item, extras = win32gui_struct.PackMENUITEMINFO(text=option_text,
                                                                 hbmpItem=option_icon,
                                                                 wID=option_id)
@@ -176,7 +177,7 @@ class SysTrayIcon(object):
                 win32gui.InsertMenuItem(menu, 0, 1, item)
 
     @staticmethod
-    def prep_menu_icon(s, icon):
+    def prep_menu_icon(self, icon):
         # 首先加载图标.
         ico_x = win32api.GetSystemMetrics(win32con.SM_CXSMICON)
         ico_y = win32api.GetSystemMetrics(win32con.SM_CYSMICON)
@@ -198,45 +199,45 @@ class SysTrayIcon(object):
 
         return hbm
 
-    def command(s, hwnd, msg, wparam, lparam):
+    def command(self, hwnd, msg, wparam, lparam):
         id = win32gui.LOWORD(wparam)
-        s.execute_menu_option(id)
+        self.execute_menu_option(id)
 
-    def execute_menu_option(s, id):
-        menu_action = s.menu_actions_by_id[id]
-        if menu_action == s.QUIT:
-            win32gui.DestroyWindow(s.hwnd)
+    def execute_menu_option(self, id):
+        menu_action = self.menu_actions_by_id[id]
+        if menu_action == self.QUIT:
+            win32gui.DestroyWindow(self.hwnd)
         else:
-            menu_action(s)
+            menu_action(self)
 
 
 class _Main:
-    def main(s):
+    def main(self):
         # s.root.resizable(0,0)
-        Application(s.root).mainloop()
+        Application(self.root).mainloop()
 
-    def __init__(s):
-        s.root = tk.Tk()
+    def __init__(self):
+        self.root = tk.Tk()
         icons = 'tmp.ico'
         hover_text = "my box plus"  # 悬浮于图标上方时的提示
-        menu_options = (('一级菜单', None, s.switch_icon),
-                        ('二级菜单', None, (('哇哈哈', None, s.switch_icon),)))
-        s.sysTrayIcon = SysTrayIcon(icons, hover_text, menu_options, on_quit=s.exit, default_menu_index=1)
-        s.root.bind("<Unmap>", lambda event: s.unmap() if s.root.state() == 'iconic' else False)
-        s.root.protocol('WM_DELETE_WINDOW', s.exit)
+        menu_options = (('一级菜单', None, self.switch_icon),
+                        ('二级菜单', None, (('哇哈哈', None, self.switch_icon),)))
+        self.sysTrayIcon = SysTrayIcon(icons, hover_text, menu_options, on_quit=self.exit, default_menu_index=1)
+        self.root.bind("<Unmap>", lambda event: self.unmap() if self.root.state() == 'iconic' else False)
+        self.root.protocol('WM_DELETE_WINDOW', self.exit)
 
     @staticmethod
-    def switch_icon(s, _sys_tray_icon, icons='tmp.ico'):
+    def switch_icon(self, _sys_tray_icon, icons='tmp.ico'):
         _sys_tray_icon.icon = icons
         _sys_tray_icon.refresh_icon()
         # 点击右键菜单项目会传递SysTrayIcon自身给引用的函数，所以这里的_sysTrayIcon = s.sysTrayIcon
 
-    def unmap(s):
-        s.root.withdraw()
-        s.sysTrayIcon.show_icon()
+    def unmap(self):
+        self.root.withdraw()
+        self.sysTrayIcon.show_icon()
 
-    def exit(s, _sys_tray_icon=None):
-        s.root.destroy()
+    def exit(self, _sys_tray_icon=None):
+        self.root.destroy()
         print('exit...')
 
 
